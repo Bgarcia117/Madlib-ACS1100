baa8c1b7c3522977fc69ec7fec331bb6488c81b9
Bryant Garcia <bryantgarcia@Bryants-MacBook-Pro.local>
2021-08-31 17:31:50 -0500

    Complete madlib using python.

---
+++ madlib.py
@@ -0,0 +1,88 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+name = input("Hi, what's your name?")
+greeting = input(f"Hi {name}, how are you doing today?")
+print(f"You're doing {greeting}? I am too, thanks for being considerate and asking.")
+hobbies = input(f"What would you say your favorite hobby is {name}?")
+print("That sounds kinda boring, but it's a hobby I guess.")
+food = input("What's your favorite food anyways?")
+print(f"{food}? Those are okay, but not as good as oreos")
+school = input(f"What school do you go to {name}?")
+print(f"{school}? Me too!")
+location = input("One last thing, where are you from?")
+print("Woah that's far from here")
+print(f"Well {name}, your favorite hobby is {hobbies}, your favorite food is {food}, you go to {school}, and you're from {location}. Based on your infomation, I think we can be friends.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
